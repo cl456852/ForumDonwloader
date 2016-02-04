44347e633960442d487340fec4f3a77bbd6b77a5
cl456852@gmail.com <cl456852@gmail.com>
2016-02-04 23:15:25 +0800

    cdddd

---
+++ JavBusDownloader/JavBusDl.cs
@@ -0,0 +1,37 @@
+﻿using Framework.abs;
+using Framework.tool;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JavBusDownloader
+{
+    public class JavBusDl : AbsLstDl
+    {
+
+        public override void Download(object obj)
+        {
+            AsynObj o = (AsynObj)obj;
+
+
+            string content = Sis001DlTool.GetHtml(o.Url, true);
+            if (content != "")
+            {
+                if (o.Path != null)
+                {
+                    DlTool.SaveFile(content, Path.Combine(o.Path, o.Url.Replace('/', '_').Replace(":", "^").Replace("?", "wenhao")) + ".htm");
+
+                }
+                JavBusSgDl sgDl = new JavBusSgDl();
+                AsynObj asynObj = new AsynObj();
+                asynObj.Path = o.Path;
+                asynObj.Content = content;
+                ThreadPool.QueueUserWorkItem(sgDl.Download, asynObj);
+            }
+        }
+    }
+}
