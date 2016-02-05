78639c32995ef871017a78ee5219634705dc1a84
cl456852 <cl456852@gmail.com>
2016-02-05 18:26:18 +0800

    Akiba-Online

---
+++ AkibaOnlineDownloader/AkibaOnlineDl.cs
@@ -0,0 +1,38 @@
+﻿using Framework.abs;
+using Framework.tool;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AkibaOnlineDownloader
+{
+    public class AkibaOnlineDl : AbsLstDl
+    {
+        public override void Download(object obj)
+        {
+            AsynObj o = (AsynObj)obj;
+
+            HttpWebRequest downloadParam = (HttpWebRequest)WebRequest.Create(Config1.EMPTY_URL);
+            downloadParam.Host = "www.akiba-online.com";
+            string content = NewDlTool.GetHtml(o.Url, true, downloadParam);
+            if (content != "")
+            {
+                if (o.Path != null)
+                {
+                    DlTool.SaveFile(content, Path.Combine(o.Path, o.Url.Replace('/', '_').Replace(":", "^").Replace("?", "wenhao")) + ".htm");
+
+                }
+                AkibaOnlineSgDl sgDl = new AkibaOnlineSgDl();
+                AsynObj asynObj = new AsynObj();
+                asynObj.Path = o.Path;
+                asynObj.Content = content;
+                ThreadPool.QueueUserWorkItem(sgDl.Download, asynObj);
+            }
+        }
+    }
+}
---
+++ AkibaOnlineDownloader/AkibaOnlineSgDl.cs
@@ -0,0 +1,73 @@
+﻿using Framework.interf;
+using Framework.tool;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AkibaOnlineDownloader
+{
+    public class AkibaOnlineSgDl : ISinglePageDonwloader
+    {
+        Regex threadRegex = new Regex("threads.* data-thumbnailurl");
+        Regex nameRegex = new Regex("<h1>.*</h1>");
+        Regex torrentRegex = new Regex("<a href=\"attachments/.*/\" target=\"_blank\">");
+        public void Download(object obj)
+        {
+            AsynObj o = (AsynObj)obj;
+            MatchCollection macthes = threadRegex.Matches(o.Content);
+            foreach(Match match in macthes)
+            {
+                string link = match.Value.Replace("/\" data-thumbnailurl", "");
+      
+                ThreadPool.QueueUserWorkItem(new AkibaOnlineSgDl().work, new AsynObj(o.Path,"https://www.akiba-online.com/" +link));
+            }
+        }
+
+        public void work(Object obj)
+        {
+            AsynObj asycObj = (AsynObj)obj;
+            try
+            {
+         
+                HttpWebRequest downloadParam = (HttpWebRequest)WebRequest.Create(Config1.EMPTY_URL);
+                downloadParam.Host = "www.akiba-online.com";
+                string content = NewDlTool.GetHtml(asycObj.Url, true, downloadParam);
+                string name = nameRegex.Match(content).Value.Replace("<h1>", "").Replace("</h1>", "");
+                string path = Path.Combine(asycObj.Path, name + ".htm").Replace("/", "").Replace("|", "");
+                MatchCollection mc = torrentRegex.Matches(content);
+                ArrayList list = new ArrayList();
+                foreach (Match match in mc)
+                {
+                    string torrentLink = "";
+                    if (match.Value.Contains("torrent"))
+                    {
+                        torrentLink = "https://www.akiba-online.com/" + match.Value.Replace("<a href=\"", "").Replace("\" target=\"_blank\">", "");
+                        list.Add(torrentLink);
+                    }
+                }
+
+                path = Config1.InvalidPathFilter(path);
+                foreach (string link in list)
+                {
+                    NewDlTool.downLoadFile(link, path + ".torrent", true, downloadParam);
+                }
+                DlTool.SaveFile(content, path);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message + "  " + asycObj.Url);
+
+                Config1.appendFile(asycObj.Url, "d:\\test\\failList.txt");
+
+                
+            }
+
+        }
+    }
+}
