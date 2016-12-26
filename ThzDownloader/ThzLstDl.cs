7ee1e65ef99a5c6c19ce2e382a54ba70b5946a05
cl456852@gmail.com <cl456852@gmail.com>
2016-12-26 23:47:05 +0800

    thz

---
+++ ThzDownloader/ThzLstDl.cs
@@ -0,0 +1,41 @@
+﻿using Framework.abs;
+using Framework.interf;
+using Framework.tool;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThzDownloader
+{
+    public class ThzLstDl : AbsLstDl
+    {
+        public override void Download(object obj)
+        {
+            AsynObj o = (AsynObj)obj;
+
+
+            string content = Sis001DlTool.GetHtml(o.Url, true, "UTF-8");
+            if (content != "")
+            {
+                //string[] contents = content.Split(new string[] { "<td align=\"center\">" }, StringSplitOptions.RemoveEmptyEntries);
+                //content = contents[1];
+                if (o.Path != null)
+                {
+                    DlTool.SaveFile(content, Path.Combine(o.Path, o.Url.Replace('/', '_').Replace(":", "^").Replace("?", "wenhao")) + ".htm");
+
+                }
+                ISinglePageDonwloader sgDl = new ThzSgDl();
+                AsynObj asynObj = new AsynObj();
+                asynObj.Path = o.Path;
+                asynObj.Content = content;
+                ThreadPool.QueueUserWorkItem(sgDl.Download, asynObj);
+            }
+
+
+        }
+    }
+}
---
+++ ThzDownloader/ThzSgDl.cs
@@ -0,0 +1,56 @@
+﻿using Framework.interf;
+using Framework.tool;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThzDownloader
+{
+    public class ThzSgDl : ISinglePageDonwloader
+    {
+        Regex nameRegex = new Regex("s xst\">.*</a>");
+        Regex nameRegex1 = new Regex(">.*?</a>");
+        Regex threadRegex = new Regex("\"thread.*\" style=");
+        Regex torrentLinkRegex = new Regex("imc_attachad-ad.html\\?aid=.*&amp");
+        public void Download(object obj)
+        {
+            AsynObj o = (AsynObj)obj;
+            try
+            {
+                string[] threads = o.Content.Split(new string[] { "新窗口打开" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string thread in threads)
+                {
+                    if (thread.Contains("桃花族论坛"))
+                        continue;
+                   
+                    string path= Path.Combine(o.Path, nameRegex1.Match(nameRegex.Match(thread).Value).Value.Replace(">", "").Replace("</a", "").Replace('/', '_').Replace(":", "^").Replace("?", "wenhao"))+".htm";
+                    string link = "http://thzbt.biz/" + threadRegex.Match(thread).Value.Replace("\"", "").Replace("style=", "");
+                    ThreadPool.QueueUserWorkItem(new ThzSgDl().work, new AsynObj(path, link));
+                    
+
+                }
+            }
+            catch (Exception e)
+            {
+                Config1.appendFile(o.Url, Path.Combine(o.Path, "failList.txt"));
+            }
+        }
+
+        void work(Object obj)
+        {
+            AsynObj asycObj = (AsynObj)obj;
+            string content = Sis001DlTool.GetHtml(asycObj.Url, true, "UTF-8");
+            DlTool.SaveFile(content, asycObj.Path);
+            Match torrentMatch= torrentLinkRegex.Match(content);
+            string torrentContent = Sis001DlTool.GetHtml("http://thzbt.biz/"+torrentMatch.Value.Replace("&amp",""), true, "UTF-8");
+            DlTool.SaveFile(torrentContent, asycObj.Path + ".htm");
+        }
+
+
+    }
+}
